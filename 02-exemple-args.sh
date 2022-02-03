6b739b94337a380386eff9d2a21813daabd96862
isx47327409 <isx47327409@j10.informatica.escoladeltreball.org>
2022-02-03 09:46:09 +0100

    scripts

---
+++ 02-exemple-args.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# isx47327409 ASXIX M01
+# 03-02-2022
+# Exemple processament arguments
+#-------------------------------
+echo '$*: ' $*
+echo '$@: ' $@
+echo '$#: ' $#
+echo '$1: ' $1
+echo '$2: ' $2
+echo '$9: ' $9
+echo '$10: ' ${10}
+echo '$11: ' ${11}
+echo '$$: ' $$
+nom='puig'
+echo "${nom}deworld"
+
+exit 0
+
---
+++ 03-exemple-ifs.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# isx47327409 ASXIX M01
+# 03-02-2022
+# Exemple if: indica si és major d'edat
+# $ prog edat
+#-------------------------------
+# 1) Validem arguments
+if [ condició ]
+then
+   accions
+fi
+
