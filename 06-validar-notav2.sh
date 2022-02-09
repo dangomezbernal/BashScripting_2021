7c943fd9a3c97a33eab17e25084f14f69b3c20c1
isx47327409 <isx47327409@j10.informatica.escoladeltreball.org>
2022-02-09 12:13:53 +0100

    scripts

---
+++ 06-validar-notav2.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+# isx47327409 ASXIX M01
+# 03-02-2022
+# Valida si un alumne está aprobat o suspés
+# $ prog nota
+#-------------------------------
+# 1) Validem arguments
+
+if [ $# -ne 1 ]
+then
+  echo "Error: numero d'arguments incorrecte"
+  echo "Usage : $0 nota"
+  exit 1
+fi
+
+  
+# 2) Validem rang de la nota
+nota=$1
+if [ $nota -lt 0 -o $nota -gt 10 ]
+then
+  echo "Error: la nota que has introduït ($nota) ha d'estar entre el 0 i el 10"
+  echo "Usage: $0 nota"	
+  exit 1
+fi
+
+# 3) Xixa 
+if [ $nota -lt 5 ]
+then 
+  echo "Amb un $nota està suspès"
+elif [ $nota -lt 7 ]
+then
+  echo "Amb un $nota està aprovat"
+elif [ $nota -lt 9 ]
+then
+  echo "Amb un $nota és un notable"
+else
+  echo "Amb un $nota és un excelent"
+fi
+exit 0
